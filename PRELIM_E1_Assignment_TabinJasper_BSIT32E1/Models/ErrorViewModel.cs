2b68be8ae2395eb6e72641884f4a21c31dbb4b72
Tabin_Jasper_BSIT32E1 <tabinjasper@gmail.com>
2024-03-01 15:36:31 +0800

    Add project files.

---
+++ PRELIM_E1_Assignment_TabinJasper_BSIT32E1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRELIM_E1_Assignment_TabinJasper_BSIT32E1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
